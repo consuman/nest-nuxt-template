b4a9fb326f5199e45f45ab26016671601f1281ed
consuman <nicolai@consuman.de>
2020-05-03 19:22:21 +0200

    Fixed production start script; Removed Ant-Design;

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,13 +28,11 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'ant-design-vue/dist/antd.css'
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/antd-ui'
   ],
   /*
   ** Nuxt.js dev-modules
